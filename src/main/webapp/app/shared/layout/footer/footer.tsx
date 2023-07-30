7f50f84e31b159e74d943b7c248b528aaafcc67e
n7eru@vulcan <edwn.njeru@gmail.com>
2023-07-30 11:55:31 +0300

    Remove translation from header and footer

--- src/main/webapp/app/shared/layout/footer/footer.tsx
+++ src/main/webapp/app/shared/layout/footer/footer.tsx
@@ -1,16 +1,14 @@
 import './footer.scss';
 
 import React from 'react';
-import { Translate } from 'react-jhipster';
+
 import { Col, Row } from 'reactstrap';
 
 const Footer = () => (
   <div className="footer page-content">
     <Row>
       <Col md="12">
-        <p>
-          <Translate contentKey="footer">Your footer</Translate>
-        </p>
+        <p>Calvary ERP ver 0.01-SNAPSHOT</p>
       </Col>
     </Row>
   </div>
